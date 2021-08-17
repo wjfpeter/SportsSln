a5cd54558fd5d64328cc634005e57186f83e3007
wjfpeter <wjfpeter7786@gmail.com>
2021-08-17 10:16:45 +0800

    模块 SportsSln 的初始签入

---
+++ SportsStore/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddRazorPages();
+            services.AddControllersWithViews();
+            services.AddDbContext<StoreDbContext>(
+                opts =>
+                {
+                    opts.UseSqlite(
+                        Configuration[
+                            "ConnectionStrings:SportsStoreConnection"]);
+                });
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+           // app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("pagination",
+                    "Products/Page{productPage}",
+                    new { Controller = "Home", action = "Index" });
+                endpoints.MapDefaultControllerRoute();
+            });
+
+            SeedData.EnsurePopulated(app);
+        }
+    }
+}
